f5c053f4c4dcfd131728e7a46d7c8acf00631003
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-10-27 19:22:17 +0530

    sdfsdfsfd

--- app/api/Auth/route.js
+++ app/api/Auth/route.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import connectmongodb from '@/app/libs/mongodb';
+import jseu from 'js-encoding-utils'
 
 // Connect to the MongoDB database
 connectmongodb("Source: Auth");
@@ -11,10 +12,14 @@ export async function GET(req) {
     const emailA = searchParams.get('email');
     const passwordA = searchParams.get('password');
 
+    const decpassword = jseu.encoder.encodeBase64(passwordA);
+
+    // console.log(decpassword);
+
 
     // console.log(uid);
 
-    const user = await User.findOne({ email: emailA, password: passwordA });
+    const user = await User.findOne({ email: emailA, password: decpassword });
 
     return new Response(JSON.stringify(user));
 }
--- app/api/createUser/route.js
+++ app/api/createUser/route.js
@@ -1,10 +1,14 @@
 import User from '../models/user'
 import { NextResponse } from 'next/server'
 import connectmongodb from '@/app/libs/mongodb'
+import jseu from 'js-encoding-utils'
 
 export async function POST(request){
     // console.log(request);
-    const {phone, address, age, email, firstname, img_url, lastname, order, password, rating, role, sold, zipcode} = await request.json()
+    var {phone, address, age, email, firstname, img_url, lastname, order, password, rating, role, sold, zipcode} = await request.json()
+
+    password = jseu.encoder.encodeBase64(password);
+
     await connectmongodb('Source: Create User')
     await User.create({address, age, email, firstname, img_url, lastname, order, password, rating, role, sold, phone, zipcode})
     return NextResponse.json({message: "user created"},{status:"200"})
