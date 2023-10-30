ee4cb96e59aefbe9c813394eddfa90f1c6df2073
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-10-31 03:24:00 +0530

    implemented posting

--- app/Components/Post/Createpost.js
+++ app/Components/Post/Createpost.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
 import './post.css'
+import axios from 'axios';
 
 export default function post() {
+
+  var uid = null
+  if (typeof window !== 'undefined') {
+    // Perform localStorage action
+    uid = localStorage.getItem('uid')
+    // console.log(uid);
+  }
+
+  const [postdata, setPostdata] = useState({
+    user_id:uid,
+    post:{
+    plant_name:'',
+    plant_description:'',
+    plant_price:0,
+    plant_climate:'',
+    plant_light:'',
+    plant_temp_minC:'',
+    plant_temp_maxC:'',
+    plant_water:'',
+    plant_use:'',
+    Plant_image:'https://www.shutterstock.com/image-illustration/palm-plants-260nw-273656024.jpg'
+    }
+  })
+
+  function SendData(){
+    axios.post('http://localhost:3000/api/createPost', postdata)
+  }
+
   return (
     <div className='post_container'>
     <div><p className='post_title'>Create Post</p></div>
@@ -9,46 +38,78 @@ export default function post() {
 
       <div className='input_template'>
       <label>Plant name </label>
-      <input type='text'></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_name: e.target.value } }))}
+        type="text"
+      />
       </div>
 
       <div className='input_template'>
       <label>Description </label>
-      <input id='desc' type='text'></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_description: e.target.value } }))}
+        type="text"
+      />
       </div>
 
       <div className='input_template'>
       <label>Price </label>
-      <input type='Number'></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_price: e.target.value } }))}
+        type="Number"
+      />
       </div>
 
       <div className='input_template'>
       <label>Climate </label>
-      <input type='text'></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_climate: e.target.value } }))}
+        type="text"
+      />
       </div>
 
       <div className='input_template'>
       <label>Ideal light </label>
-      <input type='text'></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_light: e.target.value } }))}
+        type="text"
+      />
+      </div>
+
+      <div className='input_template'>
+      <label>temp Min </label>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_temp_minC: e.target.value } }))}
+        type="Number"
+      />
+      </div>
+
+      <div className='input_template'>
+      <label>temp Max </label>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_temp_maxC: e.target.value } }))}
+        type="Number"
+      />
       </div>
 
       <div className='input_template'>
       <label>Watering </label>
-      <input type='text'></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_water: e.target.value } }))}
+        type="text"
+      />
       </div>
 
       <div className='input_template'>
       <label>Plant use </label>
-      <input type='text'></input>
-      </div>
-
-      <div className='input_template'>
-      <label>Plant Image</label>
-      <input type="file" placeholder='Post_price' accept="image/*"></input>
+      <input
+        onChange={(e) => setPostdata((prevData) => ({ ...prevData, post: { ...prevData.post, plant_use: e.target.value } }))}
+        type="text"
+      />
       </div>
 
       <div className='button_cont'>
-      <button className='post_button'>POST</button>
+      <button onClick={SendData} className='post_button'>POST</button>
       </div>
 
     </div>
---
+++ app/api/models/post.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+        user_id:String,
+        post:{
+            plant_name: String,
+            plant_description: String,
+            plant_climate: String,
+            plant_price: Number,
+            plant_temp_maxC: Number,
+            plant_temp_minC: Number,
+            plant_light: String,
+            plant_water: String,
+            plant_use: String,
+            Plant_image: String,
+        }
+})
+
+module.exports = mongoose.models.Post || mongoose.model('Post', postSchema);
